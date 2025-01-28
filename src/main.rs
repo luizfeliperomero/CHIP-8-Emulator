4b89ae65494baeeee959a67bbac510c5b67368fb
luizfeliperomero <luizfeliperomero10@gmail.com>
2025-01-28 16:00:37 -0300

    Accept roms by command args and set a default rom

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,26 @@ use display::Display;
 use keyboard::Keyboard;
 use memory::Memory;
 
+const DEFAULT_ROM: &str = "roms/spaceinvaders.ch8";
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
     debug: bool,
+    #[arg(short, long, default_value_t = DEFAULT_ROM.to_string())]
+    rom: String,
 }
 
 fn main() {
     let args = Args::parse();
     let mut memory = Memory::new();
-    let rom = "roms/tetris.ch8";
-    match memory.load(rom) {
+    match memory.load(args.rom.as_str()) {
         Ok(_) => {
             //memory.display();
         }
         Err(e) => {
-            panic!("{e}: {rom}");
+            panic!("{e}: {}", args.rom.as_str());
         }
     }
     let sdl_context = sdl2::init().unwrap();
